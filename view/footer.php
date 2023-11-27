49e8f26679a19626adae5fad3a9da1e0649e1c35
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2023-11-27 20:05:52 +0100

    refactoring js and test banner

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,6 @@
 
 <!-- Footer -->
-<footer class="container-fluid bg-color-purple-faded bottom-nav-margin d-flex flex-column justify-content-center align-items-center gap-1 py-2 mt-5">
-    <a href="/../support.php" class="link-light text-decoration-none link-opacity-75 link-opacity-100-hover">Support</a>
-    <a href="/../legal.php" class="link-light text-decoration-none link-opacity-75 link-opacity-100-hover">Mentions légales</a>
+<footer class="container-fluid bg-color-purple-faded bottom-nav-margin d-flex flex-column justify-content-center align-items-center gap-1 py-2 mt-5" style="margin-top:350px;">
+    <a href="support.php" class="link-light text-decoration-none link-opacity-75 link-opacity-100-hover">Support</a>
+    <a href="legal.php" class="link-light text-decoration-none link-opacity-75 link-opacity-100-hover">Mentions légales</a>
     <a href="#" class="link-light text-decoration-none link-opacity-75 link-opacity-100-hover">Copyright 2023 LIGHTNINGHUB CORP</a>
