6552e7bb60bfe4803fa4de8f646a2c2beb45b020
Stephane <stephane.koeniguer@gmail.com>
2023-12-11 13:08:48 +0100

    V1 dashboard

--- public/hub.php
+++ public/hub.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lightning Hub - Hub</title>
+    <link rel="icon" type="image/png" href="assets/images/logo-lightninghub.png">
     <script src="https://kit.fontawesome.com/c608f59341.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="assets/css/style.css">
