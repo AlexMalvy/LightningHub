d43ca754ac2059bb9b8c0dd1380e6ecab361f8e8
Stephane Koeniguer <103316525+StephaneKoeniguer@users.noreply.github.com>
2024-01-11 16:52:17 +0100

    Update app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,16 +6,6 @@ define('APP_BASE_PATH', realpath(__DIR__.'/../'));
 // All imports
 require_once __DIR__.'/../vendor/autoload.php';
 
-// Import function helpers
-//require_once __DIR__.'/../helpers/path_functions.php';
-//require_once __DIR__.'/../helpers/html_functions.php';
-//require_once __DIR__.'/../helpers/redirect_functions.php';
-//require_once __DIR__.'/../helpers/session_functions.php';
-//
-//// Import class helpers
-//require_once __DIR__.'/../helpers/class/App.php';
-//require_once __DIR__.'/../helpers/class/DB.php';
-//require_once __DIR__.'/../helpers/class/Auth.php';
 
 
 session_start();
