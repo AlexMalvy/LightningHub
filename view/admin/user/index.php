a2a7554044c814d5f34dcd1ca0c3eecea47c2511
Stephane Koeniguer <stephane.koeniguer@gmail.com>
2024-01-22 20:09:25 +0100

    fix bug moderatation and css home page

--- view/admin/user/index.php
+++ view/admin/user/index.php
@@ -90,16 +90,18 @@
 
 
                 <td class="text-center">
-                    <a href="#" data-bs-toggle="modal" data-bs-target="#moderation">
+                    <a href="#" data-bs-toggle="modal" data-bs-target="#moderation" data-user-id="<?php echo $user->getId();?>">
                         <img src="../assets/images/ban-solid.svg" alt="Gestion de la modération"></a>
                 </td>
             </tr>
+
             <?php endforeach; ?>
             </tbody>
         </table>
     </section>
 
     <?php require_once base_path('view/modal_moderation.php'); ?>
+
 
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- view/header_nav.php
+++ view/header_nav.php
@@ -176,16 +176,8 @@
                                 <p class="align-self-center m-0">Dashboard</p>
                                 </a>
                             </li>
-
                             <hr>
 
-                    <li class="nav-item d-flex flex-row justify-content-start align-items-center gap-2">
-                        <div>
-                            <img src="assets/images/dashboard-icon-37x37.svg" alt="" class="icon-25x25">
-                        </div>
-                        <a class="nav-link link-light hover-accent-outline focus-accent-outline"  href="admin/index.php" >Dashboard</a>
-                    </li>
-                    <hr>
                         <?php endif; ?>
                     <?php endforeach; ?>
 
