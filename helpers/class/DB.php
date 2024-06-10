8c1f77a495e85832d101268c4fd72a4ccdac2623
Alex MALVY <142165167+AlexMalvy@users.noreply.github.com>
2024-06-10 21:21:50 +0200

    .

--- helpers/class/DB.php
+++ helpers/class/DB.php
@@ -97,7 +97,7 @@ class DB {
     {
         try {
             return new PDO(
-            "mysql:host=localhost;port=3306;dbname=LightningHub",
+            "mysql:host=localhost;port=3306;dbname=LightningHub;charset='utf8mb4'",
             "lightninghub",
             "6uj)z/.(@,3+1t4AA90d",
                 [
